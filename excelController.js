b118ce403e2d20214311320353439d18b93731c7
Riina Maria Korpela <riina.mk@coditramuntana.com>
2024-01-10 21:33:42 +0100

    feat: improve name and ignore xlsx file

--- excelController.js
+++ excelController.js
@@ -3,7 +3,7 @@ const xlsx = require('xlsx');             // Step 1
 
 module.exports = {
   readExcelController: (req, res) => {
-    const workbook = xlsx.readFile('./test.xls');  // Step 2
+    const workbook = xlsx.readFile('./test.xlsx');  // Step 2
     let workbook_sheet = workbook.SheetNames;                // Step 3
     let workbook_response = xlsx.utils.sheet_to_json(        // Step 4
       workbook.Sheets[workbook_sheet[0]]
